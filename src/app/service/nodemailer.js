38bb69fe04be823576fa3bf1452f5077f03bdebd
umarkhan110 <umardraz277@gmail.com>
2024-03-04 21:54:27 +0500

    pupeeter

--- src/app/service/nodemailer.js
+++ src/app/service/nodemailer.js
@@ -1,58 +1,16 @@
-// // import nodemailer from "nodemailer";
-
-// // const email = process.env.NEXT_PUBLIC_SENDER_EMAIL;
-// // const pass = process.env.NEXT_PUBLIC_SENDER_PASSWORD;
-
-// // export const transporter = nodemailer.createTransport({
-// //   service: "outlook",
-// //   auth: {
-// //     user: email,
-// //     pass,
-// //   },
-// // });
-
-// // export const mailOptions = {
-// //   from: email,
-// //   // to: email,
-// // };
-
 import nodemailer from "nodemailer";
-
 const email = process.env.NEXT_PUBLIC_SENDER_EMAIL;
 const pass = process.env.NEXT_PUBLIC_SENDER_PASSWORD;
-
- 
-
 export const transporter = nodemailer.createTransport({
-host: "smtpout.secureserver.net",
-port: 587,
-secure: false,
-auth: {
-user: email,
-pass,
-}
+  host: "smtpout.secureserver.net",
+  port: 587,
+  secure: false,
+  auth: {
+    user: email,
+    pass,
+  },
 });
 
 export const mailOptions = {
-
-from: email,
-
+  from: email,
 };
-
-// import nodemailer from "nodemailer";
-
-// const email = process.env.NEXT_PUBLIC_SENDER_EMAIL;
-// const pass = process.env.NEXT_PUBLIC_SENDER_PASSWORD;
-
-// export const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: email,
-//     pass,
-//   },
-// });
-
-// export const mailOptions = {
-//   from: email,
-//   // to: email,
-// };
